e4f34ddcd03705c90fc038c7d5a07eee8c9f8c06
twarsop <tewarsop@gmail.com>
2024-07-24 21:00:25 +0100

    Basic form for rolling a die

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -7,15 +7,22 @@ namespace DieRollerMVC.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly Random _random;
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+        _random = new Random();
     }
 
     public IActionResult Index()
     {
         return View();
+    }
+
+    public IActionResult DieRoll()
+    {
+        return Json(_random.Next(1,7));
     }
 
     public IActionResult Privacy()
